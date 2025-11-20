c45cda49412aed8bdf5155d5caa074cb8fed1017
Csanchez <casanchez@nrmweb.com.mx>
2025-11-20 12:21:13 -0600

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sabrositadigital.mx',
+  integrations: [mdx(), sitemap({ sitemap: '/sitemap.xml' }), tailwind()],
+  devToolbar: {
+    enabled: true
+  }
+});
