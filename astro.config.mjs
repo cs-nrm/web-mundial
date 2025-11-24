8c6dbacd869ecf9a2458d5ae9aca97903b0041ed
Csanchez <casanchez@nrmweb.com.mx>
2025-11-24 15:24:12 -0600

    probando servidor

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sabrositadigital.mx',
   integrations: [mdx(), sitemap({ sitemap: '/sitemap.xml' }), tailwind()],
+
   devToolbar: {
     enabled: true
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+  output: 'server'
 });
