a116ef07203baa25ffe4c51d461a608f34312ac2
Danira <danira@MacBook-Air-de-Danira.local>
2025-12-02 09:49:53 -0600

    varios cambios y favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sabrosita 590 - 1410 AM';
-export const SITE_DESCRIPTION = 'Sabrosita 590-1410 AM somos salsa, cumbia y todos los géneros tropicales que nos convierte en la estación de radio preferida de los que les gusta bailar y gozar de la vida.';
+export const SITE_TITLE = 'La Fiesta del Fútbol';
+export const SITE_DESCRIPTION = 'La Fiesta del Fútbol es la cobertura más completa en México, con noticias, estadísticas, análisis, entrevistas y más.';
